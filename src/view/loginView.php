5b948705976e78e41c3888777e5487d59c4a6b0e
Jcalise <julien.calise@orange.fr>
2020-08-19 17:23:27 +0200

    5 - Modification GitIgnore

--- src/view/loginView.php
+++ src/view/loginView.php
@@ -1,37 +1,44 @@
 <?php
-    require_once "../assets/php/header.php"
- ?>
+require_once "../assets/php/header.php"
+?>
 
-    <div class="container h-100">
-        <div class="row justify-content-center h-100">
-            <div class="col-md-8 bg-light my-auto py-5 mx-5">
-                <form class="mx-2 mb-4">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+<div class="container">
+    <div class="row justify-content-center h-100">
+        <div class="col-md-8 my-auto py-5 mx-5">
+            <div class="card">
+                <header class="card-header">
+                    <h4 class="card-title mt-2">Log In</h4>
+                </header>
+                <article class="card-body">
+                    <form class="mx-2 mb-4">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="on" required>
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary d-flex mx-auto">Submit</button>
+                    </form>
+                    <div class="d-flex justify-content-center">
+                        <p>Don't have an account? <a href="/OnlineAdvisor/src/view/signupView.php">Sign Up</a></p>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="on">
+                    <div class="d-flex justify-content-center">
+                        <a href="#">Forgot your password?</a>
                     </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-                <div class="d-flex justify-content-center">
-                    <p>Don't have an account? <a href="/OnlineAdvisor/src/view/signupView.php">Sign Up</a></p>
-                </div>
-                <div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
-				</div>
-
+                </article>
             </div>
         </div>
     </div>
-    
+</div>
+</div>
+
 <?php
-    require_once "../assets/php/footer.php"
- ?>
+require_once "../assets/php/footer.php"
+?>
--- src/view/signupView.php
+++ src/view/signupView.php
@@ -1,7 +1,48 @@
 <?php
-    require_once "../assets/php/header.php"
- ?>
+require_once "../assets/php/header.php"
+?>
+<div class="container">
+    <div class="row justify-content-center h-100">
+        <div class="col-md-8 my-auto py-5 mx-5">
+            <div class="card">
+                <header class="card-header">
+                    <h4 class="card-title mt-2">Sign up</h4>
+                </header>
+                <article class="card-body">
+                    <form>
+                        <div class="form-row">
+                            <div class="col form-group">
+                                <label>First name </label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div> <!-- form-group end.// -->
+                            <div class="col form-group">
+                                <label>Last name</label>
+                                <input type="text" class="form-control" placeholder=" ">
+                            </div> <!-- form-group end.// -->
+                        </div> <!-- form-row end.// -->
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control" placeholder="">
+                            <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div> <!-- form-group end.// -->
+                        <div class="form-group">
+                            <label>Create password</label>
+                            <input class="form-control" type="password">
+                        </div> <!-- form-group end.// -->
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary d-flex mx-auto"> Register </button>
+                        </div> <!-- form-group// -->
+                        <small class="text-muted">By clicking the 'Sign Up' button, you confirm that you accept our <br> Terms of use and Privacy Policy.</small>
+                    </form>
+                </article> <!-- card-body end .// -->
+                <div class="border-top card-body text-center">Have an account? <a href="/OnlineAdvisor/src/view/loginView.php">Log In</a></div>
+            </div> <!-- card.// -->
+        </div> <!-- col.//-->
 
+    </div> <!-- row.//-->
+
+
+</div>
 <?php
-    require_once "../assets/php/footer.php"
- ?>
+require_once "../assets/php/footer.php"
+?>
