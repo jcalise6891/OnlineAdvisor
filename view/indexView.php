e9870340dc4ebdf81797d4635f1245e402343eb7
Jcalise <julien.calise@orange.fr>
2020-08-19 11:31:16 +0200

    2 - Initilisation Composer/BootStrap | Folder | MVC Architecture

---
+++ view/indexView.php
@@ -0,0 +1,15 @@
+<?php
+    require_once "../assets/php/header.php"
+ ?>
+
+   <div class="main h-100">
+        <div class="container">
+            <div class="row">
+                <h1>OnlineAdvisor</h1>
+            </div>
+        </div>
+    </div>
+
+<?php
+    require_once "../assets/php/footer.php"
+ ?>
